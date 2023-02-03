760f9a1f4bb077d33535fc1f8bfe1643b9c97785
HKCM <HKCM@OUTLOOK.COM>
2023-02-03 16:31:38 +0800

    通过session实现了验证码发送和用户登陆 1.通过LoginInterceptor.java和MvcConfig.java实现拦截器 2.UserController.java中添加发送验证码，登陆，以及me的功能 3.UserServiceImpl.java实现发送验证码，登陆以及me中返回用户的功能

--- hm-dianping/src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ hm-dianping/src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -1,10 +1,21 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.util.RandomUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.LoginFormDTO;
+import com.hmdp.dto.Result;
+import com.hmdp.dto.UserDTO;
 import com.hmdp.entity.User;
 import com.hmdp.mapper.UserMapper;
 import com.hmdp.service.IUserService;
+import com.hmdp.utils.RegexUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpSession;
+
+import static com.hmdp.utils.SystemConstants.USER_NICK_NAME_PREFIX;
 
 /**
  * <p>
@@ -14,7 +25,63 @@ import org.springframework.stereotype.Service;
  * @author 虎哥
  * @since 2021-12-22
  */
+@Slf4j
 @Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
 
+    @Override
+    public Result sendCode(String phone, HttpSession session) {
+        // 判断phone是否合法
+        if (RegexUtils.isPhoneInvalid(phone)) {
+            return Result.fail("手机号码不合法");
+        }
+
+        //
+        String code = RandomUtil.randomNumbers(6);
+
+        // 模拟发送验证码
+        log.debug("验证码发送成功，验证码：{}",code);
+
+        // 在session中保存验证码
+        session.setAttribute("code",code);
+
+        return Result.ok();
+    }
+
+    @Override
+    public Result login(LoginFormDTO loginForm, HttpSession session) {
+        // 判断phone是否合法
+        String phone = loginForm.getPhone();
+        if (RegexUtils.isPhoneInvalid(phone)) {
+            return Result.fail("手机号码不合法");
+        }
+
+        // 获取验证码
+        String code = loginForm.getCode();
+        Object cacheCode = session.getAttribute("code");
+
+        if (cacheCode == null || !cacheCode.toString().equals(code)){
+            return Result.fail("验证码错误");
+        }
+
+        // 检测用户是否存在
+        User user = query().eq("phone",phone).one();
+
+        if (user == null) {
+            // 如果用户不存在，则创建新用户
+            user = createUserWithPhone(phone);
+        }
+
+        // 在session中保存用户
+        session.setAttribute("user", BeanUtil.copyProperties(user, UserDTO.class));
+
+        return Result.ok();
+    }
+
+    private User createUserWithPhone(String phone) {
+        User user = new User();
+        user.setNickName(USER_NICK_NAME_PREFIX + RandomUtil.randomString(8));
+        user.setPhone(phone);
+        return user;
+    }
 }
---
+++ hm-dianping/src/main/java/com/hmdp/utils/LoginInterceptor.java
@@ -0,0 +1,33 @@
+package com.hmdp.utils;
+
+import com.hmdp.dto.UserDTO;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class LoginInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        // 获取session
+        HttpSession session = request.getSession();
+        // 判断用户是否存在
+        Object user = session.getAttribute("user");
+        // 不存在则拦截
+        if (user == null){
+            response.setStatus(401);
+            return false;
+        }
+
+        // 用户存在，将用户信息放入ThreadLocal
+        UserHolder.saveUser((UserDTO) user);
+
+        return true;
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        UserHolder.removeUser();
+    }
+}
