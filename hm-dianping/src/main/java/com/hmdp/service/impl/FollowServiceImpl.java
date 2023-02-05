d2c461b780ba56d18c8daa04587e7940e9c73fa6
HKCM <HKCM@OUTLOOK.COM>
2023-02-05 22:52:49 +0800

    基于数据库查询实现关注或取关

--- hm-dianping/src/main/java/com/hmdp/service/impl/FollowServiceImpl.java
+++ hm-dianping/src/main/java/com/hmdp/service/impl/FollowServiceImpl.java
@@ -1,9 +1,12 @@
 package com.hmdp.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.Follow;
 import com.hmdp.mapper.FollowMapper;
 import com.hmdp.service.IFollowService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.utils.UserHolder;
 import org.springframework.stereotype.Service;
 
 /**
@@ -17,4 +20,32 @@ import org.springframework.stereotype.Service;
 @Service
 public class FollowServiceImpl extends ServiceImpl<FollowMapper, Follow> implements IFollowService {
 
+    @Override
+    public Result follow(Long followUserId, Boolean isFollow) {
+        // 获取当前用户
+        Long userId = UserHolder.getUser().getId();
+
+        // 判断是关注还是取关
+        if (isFollow){
+            Follow follow = new Follow();
+            follow.setUserId(userId);
+            follow.setFollowUserId(followUserId);
+            save(follow);
+        } else {
+            remove(new QueryWrapper<Follow>().eq("user_id",userId).eq("follow_user_id",followUserId));
+        }
+
+        return Result.ok();
+    }
+
+    @Override
+    public Result isFollow(Long followUserId) {
+        // 获取当前用户
+        Long userId = UserHolder.getUser().getId();
+
+        // 查询是否关注, 有值说明关注了
+        Integer count = query().eq("user_id", userId).eq("follow_user_id", followUserId).count();
+
+        return Result.ok(count > 0);
+    }
 }
--- hm-dianping/src/main/java/com/hmdp/utils/SystemConstants.java
+++ hm-dianping/src/main/java/com/hmdp/utils/SystemConstants.java
@@ -1,7 +1,8 @@
 package com.hmdp.utils;
 
 public class SystemConstants {
-    public static final String IMAGE_UPLOAD_DIR = "D:\\lesson\\nginx-1.18.0\\html\\hmdp\\imgs\\";
+    // 将目录设置到resources/db/html/hmdp/imgs/ 下
+    public static final String IMAGE_UPLOAD_DIR = "/Users/chengke/git/Redis/hm-dianping/src/main/resources/db/html/hmdp/imgs/";
     public static final String USER_NICK_NAME_PREFIX = "user_";
     public static final int DEFAULT_PAGE_SIZE = 5;
     public static final int MAX_PAGE_SIZE = 10;
